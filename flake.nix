f3857e394a2c44872efd834ffc9a430b0fb5f2bb
Liz Ainslie <lizzy@lizainslie.dev>
2026-09-23 05:53:14 -0500

    hm: backup colliding files instead of aborting activation

--- flake.nix
+++ flake.nix
@@ -57,6 +57,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "hm-bak";
               home-manager.extraSpecialArgs = { inherit inputs; };
               home-manager.sharedModules = [
                 vicinae.homeManagerModules.default
